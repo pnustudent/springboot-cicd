0ee9a17b25b0a19d184a4e070f3bb2d6d11e1663
pnustudent <vouge@nate.com>
2024-04-03 17:06:16 +0900

    add index.html

--- src/main/java/me/shinsunyoung/springbootdeveloper/controller/UserViewController.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/UserViewController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserViewController {
 
+    @GetMapping("/")
+    public String homepage() {
+        return "index";
+    }
+
+
     @GetMapping("/login")
     public String login() {
         return "oauthLogin";
